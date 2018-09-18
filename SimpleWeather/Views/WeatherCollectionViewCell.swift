b9fa657891b899e9264dfbcbc201245bccd306f5
Jermaine Kelly <jerjunkel@gmail.com>
2018-09-18 09:07:55 -0400

    New weathercollectionView cell

---
+++ SimpleWeather/Views/WeatherCollectionViewCell.swift
@@ -0,0 +1,54 @@
+//
+//  WeatherCollectionViewCell.swift
+//  SimpleWeather
+//
+//  Created by Jermaine Kelly on 9/18/18.
+//  Copyright © 2018 Jermaine Kelly. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+    private var labeledWeatherView = LabeledWeatherView()
+    var weatherModel: WeatherViewModel? {
+        didSet {
+            updateCell()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupCell()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func updateCell() {
+        guard let viewModel = weatherModel else { return }
+        DispatchQueue.main.async {
+            self.labeledWeatherView.title = viewModel.dateFormatted
+            self.labeledWeatherView.condition = viewModel.condition
+        }
+    }
+    
+    private func setupCell() {
+        customizeCell()
+        addLabeledView()
+    }
+    
+    private func addLabeledView() {
+        addSubview(labeledWeatherView)
+        NSLayoutConstraint.activate([
+            labeledWeatherView.topAnchor.constraint(equalTo: topAnchor),
+            labeledWeatherView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            labeledWeatherView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            labeledWeatherView.trailingAnchor.constraint(equalTo: trailingAnchor)
+            ])
+    }
+    
+    private func customizeCell() {
+        backgroundColor = .white
+    }
+}
