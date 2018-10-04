60de5938e7ce46df300d48799afd44c9b53d0206
Jermaine Kelly <jerjunkel@gmail.com>
2018-10-04 10:00:02 -0400

    Code clean up

--- SimpleWeather/Managers/Task.swift
+++ SimpleWeather/Managers/Task.swift
@@ -18,7 +18,7 @@ class Task {
         session.dataTask(with: urlRequest) { (data, response, error) in
             guard let data = data else {
                 if let serverResponse = response as? HTTPURLResponse {
-                    var serverError =  ServerResponse.unknownError
+                    var serverError = ServerResponse.unknownError
                     switch serverResponse.statusCode {
                     case 500...599:
                         serverError = .unknownError
