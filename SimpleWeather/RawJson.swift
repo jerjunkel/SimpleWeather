c94a10a20be9e607e6e0c88cedd45f9ae86b19f0
Jermaine Kelly <jerjunkel@gmail.com>
2018-10-04 09:54:46 -0400

    Code clean up

--- SimpleWeather/RawJson.swift
+++ SimpleWeather/RawJson.swift
@@ -8,86 +8,7 @@
 
 import Foundation
 
-//struct RawWeather: Decodable {
-//    let coord: Coordinates
-//    let weather: [WeatherRaw]
-//    let base: String
-//    //    let main: MainRaw
-//    //    let wind: Wind
-//    //    let clouds: Clouds
-//    //    let dt: Int
-//    //    let sys: SystemInfo
-//    //    let id: Int
-//    //  //  let name: String
-//    //    let cod: Int
-//
-//    struct Coordinates: Decodable {
-//        let lon: Float
-//        let lat: Float
-//    }
-//
-//    struct WeatherRaw: Decodable {
-//        let id: Int
-//        let main: String
-//        let description: String
-//        let icon: String
-//    }
-//
-//    struct MainRaw: Decodable {
-//        let temp: Float
-//        let pressure: Float
-//        let humidity: Float
-//        let temp_max: Float
-//        let temp_min: Float
-//        let sea_level: Float
-//        let grnd_level: Float
-//
-//        var currentTempInCelsius: Float {
-//            return temp - -273.15
-//        }
-//
-//        var currentTempInFahrenheit: Float {
-//            return (temp * 9/5) - 459.67
-//        }
-//    }
-//
-//    struct Wind: Decodable {
-//        let speed: Float
-//        let deg: Int
-//    }
-//
-//    struct Clouds: Decodable {
-//        let all: Int
-//    }
-//
-//    struct SystemInfo: Decodable {
-//        let message: Float
-//        let country: String
-//        let sunrise: Int
-//        let sunset: Int
-//    }
-
-//    enum CodingKeys: CodingKey {
-//        case coord, weather, base, main, wind, clouds, dt, sys, id, name, cod
-//    }
-//
-//    init(from decoder: Decoder) throws {
-//        let containter = try decoder.container(keyedBy: CodingKeys.self)
-//
-//        coord = try? containter.decode(Coordinates.self, forKey: .coord)
-//        weather = try? containter.decode([WeatherRaw].self, forKey: .weather)
-//        base = try? containter.decode(String.self, forKey: .base)
-//        main = try? containter.decode(MainRaw.self, forKey: .main)
-//        wind = try? containter.decode(Wind.self, forKey: .wind)
-//        clouds = try? containter.decode(Clouds.self, forKey: .clouds)
-//        dt = try? containter.decode(Int.self, forKey: .dt)
-//        sys = try? containter.decode(SystemInfo.self, forKey: .sys)
-//        id = try? containter.decode(Int.self, forKey: .id)
-//        name = try? containter.decode(String.self, forKey: .name)
-//        cod = try? containter.decode(Int.self, forKey: .cod)
-//    }
-//}
-
+///A codable structure that represent the top level json that is returned from the weather api endpoint.
 struct RawForecast: Decodable {
     let city: City
     let list: [RawWeatherInfo]
@@ -104,7 +25,7 @@ struct RawForecast: Decodable {
         }
     }
 }
-
+///A codable structure that represents weather values returned from the weather api endpoint.
 struct RawWeatherInfo: Decodable {
     let dt: Double
     let main: MainRaw
