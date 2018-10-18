91b5e31a976ebacd8158114a98804890bb856ea2
Jermaine Kelly <jerjunkel@gmail.com>
2018-10-18 11:37:15 -0400

    move forecastable protocol to separate file

--- SimpleWeather/Forecast.swift
+++ SimpleWeather/Forecast.swift
@@ -8,39 +8,6 @@
 
 import Foundation
 
-protocol Forecastable: Sequence where
-    Iterator == IndexingIterator<[Element]>,
-    Element == Weather {
-    
-    var weatherArray: [Weather] { get set }
-    var weatherModelsStore: [WeatherViewModel] { get set }
-    init (weather: [Weather])
-}
-
-extension Forecastable {
-    typealias Subsquence = AnySequence
-    
-    func makeIterator() -> Iterator {
-        return weatherArray.makeIterator()
-    }
-    
-    func getWeatherAllViewModels() -> [WeatherViewModel] {
-        return weatherModelsStore
-    }
-    
-    func getWeatherModel(at index: Int) -> WeatherViewModel? {
-        guard index < weatherModelsStore.count else {
-            return nil
-        }
-        
-        return weatherModelsStore[index]
-    }
-    
-    var weatherObjects: [Weather] {
-        return weatherArray
-    }
-}
-
 ///A collection of weather objects.
 struct Forecast: Forecastable {
     var city: RawForecast.City?
---
+++ SimpleWeather/Forecastable.swift
@@ -0,0 +1,42 @@
+//
+//  Forecastable.swift
+//  SimpleWeather
+//
+//  Created by Jermaine Kelly on 10/18/18.
+//  Copyright © 2018 Jermaine Kelly. All rights reserved.
+//
+
+import Foundation
+
+protocol Forecastable: Sequence where
+    Iterator == IndexingIterator<[Element]>,
+Element == Weather {
+    
+    var weatherArray: [Weather] { get set }
+    var weatherModelsStore: [WeatherViewModel] { get set }
+    init (weather: [Weather])
+}
+
+extension Forecastable {
+    typealias Subsquence = AnySequence
+    
+    func makeIterator() -> Iterator {
+        return weatherArray.makeIterator()
+    }
+    
+    func getWeatherAllViewModels() -> [WeatherViewModel] {
+        return weatherModelsStore
+    }
+    
+    func getWeatherModel(at index: Int) -> WeatherViewModel? {
+        guard index < weatherModelsStore.count else {
+            return nil
+        }
+        
+        return weatherModelsStore[index]
+    }
+    
+    var weatherObjects: [Weather] {
+        return weatherArray
+    }
+}
